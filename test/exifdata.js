f80b2c9091be709042e9337cb5394c38c9eef537
Jake Luer <jake.luer@gmail.com>
2013-01-10 15:45:33 -0500

    test: [extract] add tests for exif/tiff/gps in result

--- test/exifdata.js
+++ test/exifdata.js
@@ -6,10 +6,48 @@ describe('.extract', function () {
   it('should recognize a jpeg', function (done) {
     exifdata.extract(img, function (err, data) {
       should.not.exist(err);
-
       data.should.have.property('type', 'JPEG');
-      console.log(data);
+      done();
+    });
+  });
 
+  it('should extract tiff data', function (done) {
+    exifdata.extract(img, function (err, data) {
+      should.not.exist(err);
+      data.should.have.property('tiff')
+        .an('object')
+        .and.include.keys(
+            'Make'
+          , 'Model'
+          , 'Orientation');
+      done();
+    });
+  });
+
+  it('should extract exif data', function (done) {
+    exifdata.extract(img, function (err, data) {
+      should.not.exist(err);
+      data.should.have.property('exif')
+        .an('object')
+        .and.include.keys(
+            'DateTimeOriginal'
+          , 'DateTimeDigitized'
+          , 'PixelXDimension'
+          , 'PixelYDimension');
+      done();
+    });
+  });
+
+  it('should extract gps data', function (done) {
+    exifdata.extract(img, function (err, data) {
+      should.not.exist(err);
+      data.should.have.property('gps')
+        .an('object')
+        .and.include.keys(
+            'GPSLatitudeRef'
+          , 'GPSLatitude'
+          , 'GPSLongitudeRef'
+          , 'GPSLongitude');
       done();
     });
   });
